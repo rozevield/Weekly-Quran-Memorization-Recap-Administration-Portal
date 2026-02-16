0671ec3c826bd2f3326bb548c18811f45c10d19a
TerminalTurtle <vanelfpmtrue@gmail.com>
2026-02-16 17:24:23 +0700

    Add comment for password hashing instructions
    Added a comment to explain password hashing usage.

--- hash.php
+++ hash.php
@@ -2,4 +2,7 @@
 $password = 'azzam123';
 $hash = password_hash($password, PASSWORD_BCRYPT);
 echo $hash; 
+
 ?>
+
+// Put your password in the 'password' variable, then run hash.php to generate the encrypted password that will later be used in phpMyAdmin.
